01d5f8ba236df12c8e7a2ce7bbd4e48b5b16cb71
taemin <xoals3094@naver.com>
2023-04-29 17:11:32 +0900

    fix: 이메일 정보 ignore 추가

--- logic/common/email/infra/EmailSender.py
+++ logic/common/email/infra/EmailSender.py
@@ -1,19 +1,20 @@
 from logic.common.email.use_case.IEmailSender import IEmailSender
 import smtplib
 from email.mime.text import MIMEText
+from config.production.email import sender_email, code
 
 
 class EmailSender(IEmailSender):
     def __init__(self):
         self.smtp = smtplib.SMTP('smtp.gmail.com', 587)
         self.smtp.starttls()
-        self.smtp.login('xoals3094@gmail.com', 'sakagnjqdgotmkzv')
+        self.smtp.login(sender_email, code)
 
     def send_auth_code(self, email, auth_code) -> str:
         msg = MIMEText(f'인증코드 : {auth_code}')
         msg['Subject'] = '인증코드'
 
-        self.smtp.sendmail('xoals3094@gmail.com', email, msg.as_string())
+        self.smtp.sendmail(sender_email, email, msg.as_string())
         self.smtp.quit()
 
         return auth_code
