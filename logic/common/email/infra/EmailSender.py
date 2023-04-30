6f3d7b8a89e1506f1eda12bec202583faddb1a4e
taemin <xoals3094@naver.com>
2023-04-30 17:04:50 +0900

    feat: 이메일 보내기 과정을 멀티프로세싱으로 비동기 처리

--- logic/common/email/infra/EmailSender.py
+++ logic/common/email/infra/EmailSender.py
@@ -2,28 +2,37 @@ from logic.common.email.use_case.IEmailSender import IEmailSender
 import smtplib
 from email.mime.text import MIMEText
 from config.production.email import sender_email, code
+from multiprocessing import Process
 
 
-class EmailSender(IEmailSender):
-    def connection(self):
+class EmailProcess(Process):
+    def __init__(self, to_email, subject, body):
+        self.from_email = sender_email
+        self.to_email = to_email
+        self.subject = subject
+        self.body = body
+        Process.__init__(self)
+
+    def run(self):
         self.smtp = smtplib.SMTP('smtp.gmail.com', 587)
         self.smtp.starttls()
         self.smtp.login(sender_email, code)
 
-    def send_auth_code(self, email, auth_code) -> str:
-        self.connection()
-        msg = MIMEText(f'인증코드 : {auth_code}')
-        msg['Subject'] = '인증코드'
+        msg = MIMEText(self.body)
+        msg['Subject'] = self.subject
 
-        self.smtp.sendmail(sender_email, email, msg.as_string())
+        self.smtp.sendmail(self.from_email, self.to_email, msg.as_string())
         self.smtp.quit()
 
+
+class EmailSender(IEmailSender):
+    def send_auth_code(self, email, auth_code) -> str:
+        subject = '인증코드'
+        body = f'인증코드 : {auth_code}'
+        EmailProcess(email, subject, body).start()
         return auth_code
 
     def send_username(self, email, username):
-        self.connection()
-        msg = MIMEText(f'아이디 : {username}')
-        msg['Subject'] = '아이디'
-
-        self.smtp.sendmail('xoals3094@gmail.com', email, msg.as_string())
-        self.smtp.quit()
+        subject = '아이디'
+        body = f'아이디 : {username}'
+        EmailProcess(email, subject, body).start()
