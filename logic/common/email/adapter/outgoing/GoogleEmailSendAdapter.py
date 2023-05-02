922ba12b8b6587165e6eebd35ed2d1197d9980ce
taemin <xoals3094@naver.com>
2023-05-02 19:57:58 +0900

    feat: 이메일 전송 비동기 처리

--- logic/common/email/adapter/outgoing/GoogleEmailSendAdapter.py
+++ logic/common/email/adapter/outgoing/GoogleEmailSendAdapter.py
@@ -1,8 +1,28 @@
 import smtplib
 from email.mime.text import MIMEText
+from multiprocessing import Process
 from config.production.email import sender_email, code
-
 from logic.common.email.application.port.outgoing.EmailSender import EmailSender
+
+
+class EmailProcess(Process):
+    def __init__(self, from_email, to_email, subject, body):
+        self.from_email = from_email
+        self.to_email = to_email
+        self.subject = subject
+        self.body = body
+        Process.__init__(self)
+
+    def run(self):
+        smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        smtp.starttls()
+        smtp.login(self.from_email, code)
+
+        msg = MIMEText(self.body)
+        msg['Subject'] = self.subject
+
+        smtp.sendmail(self.from_email, self.to_email, msg.as_string())
+        smtp.quit()
 
 
 class GoogleEmailSender(EmailSender):
@@ -10,16 +30,5 @@ class GoogleEmailSender(EmailSender):
         self.from_email = sender_email
         self.app_code = code
 
-    def connection(self):
-        smtp = smtplib.SMTP('smtp.gmail.com', 587)
-        smtp.starttls()
-        smtp.login(self.from_email, self.app_code)
-        return smtp
-
     def send(self, to, subject, body):
-        smtp = self.connection()
-        msg = MIMEText(body)
-        msg['Subject'] = subject
-
-        smtp.sendmail(self.from_email, to, msg.as_string())
-        smtp.quit()
+        EmailProcess(self.from_email, to, subject, body).start()
