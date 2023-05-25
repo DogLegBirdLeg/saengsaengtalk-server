a47a47344f88676540052b497fb9e7aebf57a65b
taemin <xoals3094@naver.com>
2023-05-26 00:20:26 +0900

    fix: update_many

--- logic/user/adapter/outgoing/UserAdapter.py
+++ logic/user/adapter/outgoing/UserAdapter.py
@@ -23,36 +23,36 @@ class MongoDBUserDao(UserDao):
     def update_pw(self, user_id, pw):
         find = {'_id': user_id}
         update = {'$set': {'pw': pw}}
-        self.db.user.update(find, update)
+        self.db.user.update_many(find, update)
 
         find = {'user._id': user_id}
         update = {'$set': {'user.pw': pw}}
-        self.db.token.update(find, update)
+        self.db.token.update_many(find, update)
 
     def update_email(self, user_id, email):
         find = {'_id': user_id}
         update = {'$set': {'email': email}}
-        self.db.user.update(find, update)
+        self.db.user.update_many(find, update)
 
         find = {'user._id': user_id}
         update = {'$set': {'user.email': email}}
-        self.db.token.update(find, update)
+        self.db.token.update_many(find, update)
 
     def update_nickname(self, user_id, nickname):
         find = {'_id': user_id}
         update = {'$set': {'nickname': nickname}}
-        self.db.user.update(find, update)
+        self.db.user.update_many(find, update)
 
         find = {'user._id': user_id}
         update = {'$set': {'user.nickname': nickname}}
-        self.db.token.update(find, update)
+        self.db.token.update_many(find, update)
 
     def update_account_number(self, user_id, account_number):
         find = {'_id': user_id}
         update = {'$set': {'account_number': account_number}}
-        self.db.user.update(find, update)
+        self.db.user.update_many(find, update)
 
         find = {'user._id': user_id}
         update = {'$set': {'user.account_number': account_number}}
-        self.db.token.update(find, update)
+        self.db.token.update_many(find, update)
 
